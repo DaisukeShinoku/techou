5d337d77247238cfb27d60817ad06484c4666701
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2024-06-27 19:00:53 +0900

    Enumをハッシュに変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
 
-  enum :status, %i[todo wip done], validate: true
+  enum :status, { todo: 0, wip: 1, done: 2 }, validate: true
 end
